e1c2decea362cd447cf5bf36d8b2b89d1e752a26
root <shiv.dadlani@gmail.com>
2014-01-27 19:42:11 +0000

    removing project specific files

--- common/utils.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import current_app
-from itsdangerous import URLSafeTimedSerializer
-
-
-def get_signer():
-    secret = current_app.config['SECRET_KEY']
-    s = URLSafeTimedSerializer(secret)
-    return s
--- run.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from application import create_app
-app = create_app('settings')
-app.run()
